02c586d614961d121b05a5b9c4920b153a35fdb1
andreadjemison <45560933+andreadjemison@users.noreply.github.com>
2021-04-30 23:41:27 -0400

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    googleClientID: '203288845918-tc5nqnepcdabpjhip1jck80qe1bug133.apps.googleusercontent.com',
-    googleClientSecret: 'ipEMzmYs8E5V7XaLztefWtUI',
-    mongoURI: 'mongodb+srv://dbPHA:@nDr3@263732@phassociates.llvsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey: 'jhgftrsyrehtdx'
-}
